7b96b38ca93e8ec2fc7af38628d76b2cf0533824
a19hu <a19hu20osh1jkl@gmail.com>
2024-07-03 01:14:46 +0530

    all things are respondive

--- src/Components/Pages/CounsellorTeam.jsx
+++ src/Components/Pages/CounsellorTeam.jsx
@@ -23,6 +23,36 @@ export default function CounsellorTeam() {
                 <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> anjalibhatia@iitj.ac.in</div>
             </div>
         </div>
+        <div className='info_about_team'>
+            <img src={anjali} alt="" className='team_img' />
+            <div className='team_details'>
+                <div className='team_details_name'>Dr Anjali Bhatia</div>
+                <div className='team_content'>Counsellor</div>
+                <div className='team_content'>PhD Psychology</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 9897347663</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> anjalibhatia@iitj.ac.in</div>
+            </div>
+        </div>
+        <div className='info_about_team'>
+            <img src={anjali} alt="" className='team_img' />
+            <div className='team_details'>
+                <div className='team_details_name'>Dr Anjali Bhatia</div>
+                <div className='team_content'>Counsellor</div>
+                <div className='team_content'>PhD Psychology</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 9897347663</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> anjalibhatia@iitj.ac.in</div>
+            </div>
+        </div>
+        <div className='info_about_team'>
+            <img src={anjali} alt="" className='team_img' />
+            <div className='team_details'>
+                <div className='team_details_name'>Dr Anjali Bhatia</div>
+                <div className='team_content'>Counsellor</div>
+                <div className='team_content'>PhD Psychology</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 9897347663</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> anjalibhatia@iitj.ac.in</div>
+            </div>
+        </div>
         
        </div>
         </div>
