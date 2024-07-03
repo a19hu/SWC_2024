ef39ce7a31a373d9b35d99d2fba11e880024d289
a19hu <a19hu20osh1jkl@gmail.com>
2024-07-03 12:04:38 +0530

    festival add

---
+++ src/Components/Pages/Festival.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import '../Style/fastival.css'
+import { Link } from 'react-router-dom'
+import Ignus from '../part/Ignus'
+import Varchas from '../part/Varchas'
+import Spandan from '../part/Spandan'
+import Nimble from '../part/Nimble'
+import Prometeo from '../part/Prometeo'
+export default function Festival() {
+    const [current,setcurrent]=useState(0)
+
+    const functions=[
+      < Ignus/>,
+      <Varchas/>,
+      <Spandan/>,
+      <Nimble/>,
+      <Prometeo/>
+    ]
+  return (
+    <div className='festival'>
+      <div className='box_navbar'>
+        <Link onClick={()=>setcurrent(0)} className={current === 0 ? "Active" : ""}>Ignus</Link>
+        <Link onClick={()=>setcurrent(1)} className={current === 1 ? "Active" : ""}>Varchas</Link>
+        <Link onClick={()=>setcurrent(2)} className={current === 2 ? "Active" : ""}>Spandan</Link>
+        <Link onClick={()=>setcurrent(3)} className={current === 3 ? "Active" : ""}>Nimble</Link>
+        <Link onClick={()=>setcurrent(4)} className={current === 4 ? "Active" : ""}>Prometeo</Link>
+      </div>
+      { functions[current]}
+    </div>
+  )
+}
---
+++ src/Components/part/Varchas.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../Style/fastival.css'
+import fest from '../../Images/image 3.png'
+export default function Varchas() {
+  return (
+    <>
+      
+     <div className='box'>
+         <div className='fest_context'>
+           Varchas is the sports fest of IIT Jodhpur organised every year in the month of February. This year it was organised from 30th Oct to 2nd Nov. Varchas celebrates the spirit of sportsmanship and serves as a platform to showcase countless hours of perspiration put in by college teams to achieve excellence in sports. Varchas was first conducted in the year 2011, with a vision of promoting sports among the colleges of India, particularly Rajasthan and providing the athletes in India a platform to showcase their talent.
+               </div>
+         <br />
+         <br />
+         <div className='fest_context'>
+          Competitions are held in the fields of football, cricket, table tennis, lawn tennis, badminton, squash, volleyball, basketball and athletics in national level stadiums of Jodhpur. Varchas promotes healthy competition and is a great opportunity for the teams to prove their mettle in their respective sports.
+               </div>
+          <br />
+         <br />
+         <div className='fest_context'>
+         Moreover, being the educated core of the country, the students of IIT Jodhpur try to fulfil their social duties through "Soch -The Social Aspect of Varchas". Soch is a platform where we take up pertinent social issues and try to find possible solutions through discussions and debates; spread awareness and conduct drives to involve people spanning across varied backgrounds. We attempt to create a profound social impact for the amelioration of the society.
+                </div>
+          <br />
+         <br />
+        
+         <div className='fest_context'>
+           To know more about Varchas Click Here
+         </div>
+      </div>
+      <div className='box'>
+        <div className='fest_image'>
+
+         <img src={fest} alt=""  />
+        </div>
+      </div>
+    </>
+  )
+}
