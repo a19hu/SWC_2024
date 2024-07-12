32accfe8acb88715d51dc1cc4c0f07c6391612b6
sarahfatima1205 <142889410+sarahfatima1205@users.noreply.github.com>
2024-07-12 11:18:23 +0530

    adding team details in the Counsellor Teams Page

--- src/Components/Pages/CounsellorTeam.jsx
+++ src/Components/Pages/CounsellorTeam.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../Style/Councellerteam.css'
 import anjali from '../../Images/anjali_bhatia.jpg'
+import prasad from '../../Images/prasad_gadkari.jpg'
+import akash from '../../Images/akash_vishwakarma.jpeg'
+import nabeelah from '../../Images/Nabeelah_Siddiqui.jpg'
 export default function CounsellorTeam() {
   return (
     <div className='Councellerteam'>
@@ -24,33 +27,33 @@ export default function CounsellorTeam() {
             </div>
         </div>
         <div className='info_about_team'>
-            <img src={anjali} alt="" className='team_img' />
+            <img src={prasad} alt="" className='team_img' />
             <div className='team_details'>
-                <div className='team_details_name'>Dr Anjali Bhatia</div>
+                <div className='team_details_name'>Dr Prasad Gadkari</div>
                 <div className='team_content'>Counsellor</div>
-                <div className='team_content'>PhD Psychology</div>
-                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 9897347663</div>
-                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> anjalibhatia@iitj.ac.in</div>
+                <div className='team_content'></div>
+                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 98223 55796</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> prasadgadkari@osstaff.iitj.ac.in</div>
             </div>
         </div>
         <div className='info_about_team'>
-            <img src={anjali} alt="" className='team_img' />
+            <img src={akash} alt="" className='team_img' />
             <div className='team_details'>
-                <div className='team_details_name'>Dr Anjali Bhatia</div>
+                <div className='team_details_name'>Dr Akash Verma</div>
                 <div className='team_content'>Counsellor</div>
-                <div className='team_content'>PhD Psychology</div>
-                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 9897347663</div>
-                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> anjalibhatia@iitj.ac.in</div>
+                <div className='team_content'>M.Phil Clinical Psychology (RCI)</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 9651530172</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> akashvishwakarma@iitj.ac.in</div>
             </div>
         </div>
         <div className='info_about_team'>
-            <img src={anjali} alt="" className='team_img' />
+            <img src={nabeelah} alt="" className='team_img' />
             <div className='team_details'>
-                <div className='team_details_name'>Dr Anjali Bhatia</div>
-                <div className='team_content'>Counsellor</div>
-                <div className='team_content'>PhD Psychology</div>
-                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 9897347663</div>
-                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> anjalibhatia@iitj.ac.in</div>
+                <div className='team_details_name'>Ms Nabeelah Siddiqui</div>
+                <div className='team_content'>Well-being Moderator</div>
+                <div className='team_content'>M.A. Clinical Psychology</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Contact :</span> +91 8770205067</div>
+                <div className='team_content'><span style={{fontWeight:600}}> Mail : </span> nabeelah@osstaff.iitj.ac.in</div>
             </div>
         </div>
         
