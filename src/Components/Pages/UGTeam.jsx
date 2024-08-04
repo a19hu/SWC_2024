b213ba61cf1fbb5e56e460625974fd4091e23935
Tanmay Daga <tanmay06daga@gmail.com>
2024-08-04 20:05:54 +0530

    connected fa to backend

--- src/Components/Pages/UGTeam.jsx
+++ src/Components/Pages/UGTeam.jsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../Style/ugteam.css";
-import { FaWhatsapp } from "react-icons/fa";
-import { MdOutlineEmail } from "react-icons/md";
-import { IoLogoInstagram } from "react-icons/io5";
-import { TiSocialLinkedin } from "react-icons/ti";
-import { Link } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { BASE_URL } from "../../data/constant";
 import useFetch from "../../Hooks/useFetch";
+import { renderTeamMember } from "../../utils/renderTeamMember";
 // Assuming you have a placeholder image
 
 export default function UGTeam() {
@@ -30,49 +26,6 @@ export default function UGTeam() {
   const { error: studentGuideError, data: studentGuides } =
     useFetch(ugStudentGuidUrl);
 
-  const renderTeamMember = (member) => (
-    <div
-      className="info_about_team"
-      data-aos="fade-up"
-      data-aos-duration="1000"
-      key={member.email}
-    >
-      <img
-        src={member.photo || "https://placehold.co/600x400"}
-        alt={member.name}
-        className="team_img"
-      />
-      <div className="team_details">
-        <div className="team_details_name">{member.name}</div>
-        <div className="team_content">
-          <FaWhatsapp color="green" size={20} style={{ marginRight: 7 }} />
-          {member.phone}
-        </div>
-        <div className="team_content">
-          <span style={{ fontWeight: 600 }}> Hometown :</span> {member.hometown}
-        </div>
-        <div className="team_content">
-          <span style={{ fontWeight: 600 }}> Branch : </span> {member.branch}
-        </div>
-        <div className="team_content_small">{member.description}</div>
-        <div className="team_content_icon">
-          <Link to={`mailto:${member.email}`}>
-            <MdOutlineEmail size={20} />
-          </Link>
-          {member.instagram && (
-            <Link to={`https://instagram.com/${member.instagram}`}>
-              <IoLogoInstagram size={19} />
-            </Link>
-          )}
-          {member.linkedin && (
-            <Link to={`https://linkedin.com/in/${member.linkedin}`}>
-              <TiSocialLinkedin size={22} />
-            </Link>
-          )}
-        </div>
-      </div>
-    </div>
-  );
 
   return (
     <div className="ugteam">
